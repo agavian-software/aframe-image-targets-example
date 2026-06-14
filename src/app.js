e24ab2b29f48403c4ad6083ca38ca132893a4b1d
agavian-software <agaviansoftware@gmail.com>
2026-06-14 16:00:13 +0530

    fix video playing

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
       require('../image-targets/model-target.json'),
-      require('../image-targets/video-target.json'),
+      require('../image-targets/Manoj_A3_plus.json'),
     ],
   })
 }
